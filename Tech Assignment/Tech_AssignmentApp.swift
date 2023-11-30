cf680a842046f14fddfc84e1dc4e92d3d2e0c8d7
Kenneth Yong <kennethyong95@hotmail.com>
2023-11-30 14:15:56 +0800

    Initial Commit

---
+++ Tech Assignment/Tech_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_AssignmentApp.swift
+//  Tech Assignment
+//
+//  Created by Kenneth Yong on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Tech_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
